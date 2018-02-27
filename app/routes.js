587aa21138513d4ad318a90730e149f3268adf69
AlinP25 <alinp2508@gmail.com>
2018-02-27 23:09:21 +0200

    Added Polls view and Mypolls. Can create polls

--- app/routes.js
+++ app/routes.js
@@ -1,76 +1,106 @@
-const Poll = require('./models/poll');
+const Poll = require("./models/poll");
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => {
-    if (req.isAuthenticated()) {
-      res.render('index', {user: req.user});
-    } else {
-      res.render('index');
-    }
+  app.get("/", (req, res) => {
+    Poll.find({}, (err, polls) => {
+      if (err) {
+        console.log(err);
+        res.send("Server problems! ERROR 404!");
+      }
+      res.render("index", { user: req.user, pollsList: polls });
+    });
   });
 
-  app.get('/login', (req, res) => {
-    res.render('login', {message: req.flash('loginMessage')});
+  app.get("/login", (req, res) => {
+    res.render("login", { message: req.flash("loginMessage") });
   });
 
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true
+    })
+  );
 
-  app.get('/signup', (req, res) => {
-    res.render('signup', {message: req.flash('signupMessage')});
+  app.get("/signup", (req, res) => {
+    res.render("signup", { message: req.flash("signupMessage") });
     // res.render('signup');
   });
 
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/',
-    failureRedirect: '/signup',
-    failureFlash: true
-  }));
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/",
+      failureRedirect: "/signup",
+      failureFlash: true
+    })
+  );
 
-  app.get('/logout', (req, res) => {
+  app.get("/logout", (req, res) => {
     req.logout();
     req.session.destroy();
-    res.redirect('/');
+    res.redirect("/");
   });
 
-  app.get('/newpoll', (req, res) => {
+  app.get("/newpoll", (req, res) => {
     if (req.isAuthenticated()) {
-      res.render('newpoll', {user: req.user});
+      res.render("newpoll", { user: req.user });
     } else {
-      res.redirect('/');
+      res.redirect("/");
     }
   });
 
-  app.post('/newpoll', (req, res) => {
+  app.post("/newpoll", (req, res) => {
     const newPoll = new Poll();
     newPoll.title = req.body.title;
-    newPoll.authorId = req.user._id;
-    newPoll.options = req.body.option.filter((opt) => opt != '').map((opt) => {
-      const optAux = [];
-      if (opt != "") {
-        return {
-          option: opt,
+    newPoll.authorID = req.user._id;
+    if (!req.body.option) {
+      return res.redirect('/');
+    }
+    if (typeof req.body.option === "string" && req.body.option != "") {
+      newPoll.options = [
+        {
+          option: req.body.option,
           votes: 0
-        };
-      }
-    });
-    
-    newPoll.save((err) => {
+        }
+      ];
+    } else {
+      newPoll.options = req.body.option.filter(opt => opt != "").map(opt => {
+        const optAux = [];
+        if (opt != "") {
+          return {
+            option: opt,
+            votes: 0
+          };
+        }
+      });
+    }
+
+    newPoll.save(err => {
       if (err) {
         console.log(err);
       }
-    })
-    res.redirect('/');
+    });
+    res.redirect("/");
   });
-}
+
+  app.get("/mypolls", (req, res) => {
+    Poll.find({ authorID: req.user._id }, (err, polls) => {
+      if (err) {
+        console.log(err);
+        res.send("Server problems! ERROR 404!");
+      }
+      res.render("mypolls", { user: req.user, pollsList: polls });
+    });
+  });
+};
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
 
-  res.redirect('/');
+  res.redirect("/");
 }
